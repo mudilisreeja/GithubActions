8dd32666cd9b3ae397afa4c6dee519bece7ee9ac
mudilisreeja <146209726+mudilisreeja@users.noreply.github.com>
2025-04-10 14:30:01 +0530

    largest.py

---
+++ src/largest.py
@@ -0,0 +1,21 @@
+def find_largest(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+#This is test.py
+import unittest
+from main import find_largest
+
+class TestLargestNumber(unittest.TestCase):
+    def test_largest(self):
+        self.assertEqual(find_largest(10, 20, 30)
+        self.assertEqual(find_largest(50, 20, 10)
+        self.assertEqual(find_largest(-1, -5, -3)
+        self.assertEqual(find_largest(5, 5, 5)
+        self.assertEqual(find_largest(7, 9, 9)
+
+if __name__ == '__main__':
+    unittest.main()
